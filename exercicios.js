346f85be4a0296d403ab4815387dee9a3f0dd2c7
barbarassantoos <131317346+barbarassantoos@users.noreply.github.com>
2023-05-21 20:52:49 -0300

    Revert "Projeto Finalizado"

--- exercicios.js
+++ exercicios.js
@@ -1,13 +1,11 @@
 // EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
 
 // EXERCÍCIO 0A
-function soma(num1,num2) {
+function soma(num1, num2) {
+  // implemente sua lógica aqui
   return num1 + num2
 }
-const numero1 = 2
-const numero2 = 10
-const resultadoSoma = soma(numero1,numero2)
-console.log(resultadoSoma);
+
 // EXERCÍCIO 0B
 function imprimeMensagem() {
   // implemente sua lógica aqui
@@ -15,121 +13,80 @@ function imprimeMensagem() {
 
   console.log(mensagem)
 }
-imprimeMensagem("óla mundo")
+
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
 function calculaAreaRetangulo() {
- 
-  let altura = Number (prompt('Digite a altura do seu retangulo:'))
-  let largura = Number (prompt('Digite a largura do seu retangulo:'))
-  const calculaAreaRetangulo = largura * altura
-  console.log(calculaAreaRetangulo)
+  // implemente sua lógica aqui
+
 }
-
-
 
 // EXERCÍCIO 02
 function imprimeIdade() {
-  let anoAtual = Number(prompt("Qual o ano atual?"))
-  let anoDeNascimento = Number(prompt("Qual o seu ano de nascimento?"))
-  
-const idade = anoAtual - anoDeNascimento
-console.log(`Sua idade é ${idade} anos.`)
+  // implemente sua lógica aqui
+
 }
-imprimeIdade()
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
-  const imc = peso / (altura * altura);
-  return imc;
-}
+  // implemente sua lógica aqui
 
-const peso = 85;
-const altura = 1.8;
-const resultadoIMC = calculaIMC(peso, altura);
-console.log(resultadoIMC);
+}
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
- const nome = prompt('Qual o seu nome?')
- const idade = prompt ('Qual a sua idade?')
- const email = prompt ('Qual o seu e-mail?')
-  console.log(`O meu nome é ${nome}, tenho ${idade}, e o meu e-mail é ${email}`);
+  // implemente sua lógica aqui
+  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+
 }
-imprimeInformacoesUsuario()
+
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
+  // implemente sua lógica aqui
 
-  const cor1 = prompt('Digite sua primeira cor favorita:');
-  const cor2 = prompt('Digiteoi sua segunda cor favorita:');
-  const cor3 = prompt('Digite sua terceira cor favorita:');
-  const tresCoresFavoritas =[cor1,cor2,cor3] 
-  console.log (tresCoresFavoritas);
 }
-imprimeTresCoresFavoritas()
+
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
-  const stringMaiuscula = string.toUpperCase()
-  return stringMaiuscula;
+  // implemente sua lógica aqui
 
 }
-let frase = "Olá mundo"
-const stringMaiuscula = retornaStringEmMaiuscula(frase)
-console.log(frase);
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
-  const ingressosVendidos= custo/valorIngresso
-  return ingressosVendidos
-}
-const custo = 50000
-  const valorIngresso = 500
-  const resultado = calculaIngressosEspetaculo (custo, valorIngresso)
-console.log(resultado);
+  // implemente sua lógica aqui
 
+}
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
-  return string1.length === string2.length;
-}
+  // implemente sua lógica aqui
 
-const string1 = "abc";
-const string2 = "cdef";
-const mesmoTamanho = checaStringsMesmoTamanho(string1, string2);
-console.log(mesmoTamanho);
+}
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
-return array [0];
+  // implemente sua lógica aqui
+
 }
-const arrayNumber = [8,9,11,25]
-const primeiroElemento = retornaPrimeiroElemento(arrayNumber)
-console.log(primeiroElemento);
+
 // EXERCÍCIO 10
-
 function retornaUltimoElemento(array) {
-  return array [array.lengty-1];
-}
-const arrayNumber2 = [8,9,11,25]
-const primeiroElemento2 = retornaUltimoElemento(arrayNumber2)
-console.log(primeiroElemento2)
+  // implemente sua lógica aqui
 
+}
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
+  // implemente sua lógica aqui
+
 }
-console.log();
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  // implemente sua lógica aqui
 
-    const lowerString1 = string1.toLowerCase();
-    const lowerString2 = string2.toLowerCase();
-  
-   
-    return lowerString1 === lowerString2;
-  
 }
 
 // EXERCÍCIO 13
